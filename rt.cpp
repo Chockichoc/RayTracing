9c2b9f8685a7b2f972fac8cd0aa6e8867dd27a11
Chockichoc <alexandre.yos@gmail.com>
2019-10-04 10:17:15 +0200

    end of the tutorial

--- rt.cpp
+++ rt.cpp
@@ -1,21 +1,102 @@
 #include <iostream>
-#include "vec3.h"
+#include <fstream>
+#include "sphere.h"
+#include "hitablelist.h"
+#include "camera.h"
+#include "material.h"
+
+
+vec3 color(const ray& r, hitable *world, int depth)
+{
+    hit_record rec;
+    if(world->hit(r, 0.001, MAXFLOAT, rec))
+    {
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        {
+            return attenuation*color(scattered, world, depth+1);
+        }
+        else 
+        {
+            return vec3(0, 0, 0);
+        }
+    }
+    else
+    {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    }
+}
+
+hitable* random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(), 0.2, b+ 0.9*drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                }
+                else if (choose_mat < 0.95) {
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1+drand48()), 0.5*(1+drand48()), 0.5*(1+drand48())), 0.5*drand48()));
+                }
+                else {
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+    
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    
+    return new hitable_list(list, i);
+}
 
 int main()
 {
-    int nx = 200;
-    int ny = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+     
+    std::ofstream of{"image.ppm", std::ios_base::binary};
+    int nx = 80;
+    int ny = 40;
+    int ns = 10;
+    of << "P3\n" << nx << " " << ny << "\n255\n";
+    
+
+    hitable *world = random_scene();
+    vec3 lookfrom(20, 5, 10);
+    vec3 lookat(0, 1, 0);
+    float dist_to_focus = (lookfrom-lookat).length();
+    float aperture = 0.2;
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), 10, float(nx)/float(ny), aperture, dist_to_focus);
     for (int j = ny-1; j >=0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            vec3 col(float(i) / float(nx), float(j)/float(ny), 0.2);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++)
+            {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world, 0);
+            }
+            
+            col /= float(ns);
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            of << ir << " " << ig << " " << ib << "\n";
         }
     }
+
 }
     
